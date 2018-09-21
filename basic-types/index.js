8f94e56f46c39e896fcef49ff2addb3783e65d27
徐弘历 <hongli.xu@alphainsight.ai>
2018-09-21 18:36:48 +0800

    up

---
+++ basic-types/index.js
@@ -0,0 +1,22 @@
+// 布尔值
+var isD = false;
+console.log(isD, 'isD');
+// 数字
+var decLiteral = 6;
+var hexLiteral = 0xf00d;
+var binaryLiteral = 10;
+var octalLiteral = 484;
+console.log(hexLiteral, 'hexLiteral');
+console.log(binaryLiteral, 'binaryLiteral');
+console.log(octalLiteral, 'octalLiteral');
+// 字符串
+var str = 'str';
+var user = 'XuHongLi';
+var age = 24;
+var sentence = "Hello my name is " + user + " I' ii " + age + " years old next month.";
+console.log(sentence, 'sentence');
+// 数组
+var list = [1, 2, 3];
+var list2 = ['s'];
+console.log(list, 'list');
+console.log(list2, 'list2');
---
+++ basic-types/index.ts
@@ -0,0 +1,29 @@
+// 布尔值
+let isD:boolean = false;
+console.log(isD, 'isD');
+
+// 数字
+
+let decLiteral: number = 6
+let hexLiteral: number = 0xf00d
+let binaryLiteral: number = 0b1010
+let octalLiteral: number = 0o744
+
+console.log(hexLiteral, 'hexLiteral')
+console.log(binaryLiteral, 'binaryLiteral')
+console.log(octalLiteral, 'octalLiteral')
+
+
+// 字符串
+let str: string = 'str'
+
+let user: string = 'XuHongLi'
+let age: number = 24
+let sentence = `Hello my name is ${user} I' ii ${age} years old next month.`
+console.log(sentence, 'sentence')
+// 数组
+let list:number[] = [1,2,3]
+let list2:Array<string> = ['s']
+
+console.log(list,'list')
+console.log(list2,'list2')
