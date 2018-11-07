98367bd8505379bf4b54e741a05e845a857c8fc5
徐弘历 <hongli.xu@alphainsight.ai>
2018-11-07 15:34:39 +0800

    update

--- basic-types/index.js
+++ basic-types/index.js
@@ -1,22 +1,40 @@
 // 布尔值
 var isD = false;
-console.log(isD, 'isD');
 // 数字
 var decLiteral = 6;
 var hexLiteral = 0xf00d;
 var binaryLiteral = 10;
 var octalLiteral = 484;
-console.log(hexLiteral, 'hexLiteral');
-console.log(binaryLiteral, 'binaryLiteral');
-console.log(octalLiteral, 'octalLiteral');
+console.log(hexLiteral, "hexLiteral");
+console.log(binaryLiteral, "binaryLiteral");
+console.log(octalLiteral, "octalLiteral");
 // 字符串
-var str = 'str';
-var user = 'XuHongLi';
+var str = "str";
+var user = "XuHongLi";
 var age = 24;
 var sentence = "Hello my name is " + user + " I' ii " + age + " years old next month.";
-console.log(sentence, 'sentence');
+console.log(sentence, "sentence");
 // 数组
 var list = [1, 2, 3];
-var list2 = ['s'];
-console.log(list, 'list');
-console.log(list2, 'list2');
+var list2 = ["s"];
+console.log(list, "list");
+console.log(list2, "list2");
+// 元组
+var x;
+x = ["string", 123]; // ok
+// x = [123,'string'] // Error
+// 访问跨界元素 会使用联合类型替代
+x[3] = "hello";
+// x[2] = true // 布尔不是(string | number)
+console.log(x, "x");
+// 枚举
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 5] = "Blue";
+})(Color || (Color = {}));
+var Colorq = Color[2];
+console.log(Colorq, 'colorQ');
+var colorB = Color[5];
+console.log(colorB, 'colorB');
--- basic-types/index.ts
+++ basic-types/index.ts
@@ -1,29 +1,50 @@
 // 布尔值
-let isD:boolean = false;
-console.log(isD, 'isD');
+let isD: boolean = false;
 
 // 数字
 
-let decLiteral: number = 6
-let hexLiteral: number = 0xf00d
-let binaryLiteral: number = 0b1010
-let octalLiteral: number = 0o744
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
 
-console.log(hexLiteral, 'hexLiteral')
-console.log(binaryLiteral, 'binaryLiteral')
-console.log(octalLiteral, 'octalLiteral')
-
+console.log(hexLiteral, "hexLiteral");
+console.log(binaryLiteral, "binaryLiteral");
+console.log(octalLiteral, "octalLiteral");
 
 // 字符串
-let str: string = 'str'
+let str: string = "str";
 
-let user: string = 'XuHongLi'
-let age: number = 24
-let sentence = `Hello my name is ${user} I' ii ${age} years old next month.`
-console.log(sentence, 'sentence')
+let user: string = "XuHongLi";
+let age: number = 24;
+let sentence = `Hello my name is ${user} I' ii ${age} years old next month.`;
+console.log(sentence, "sentence");
 // 数组
-let list:number[] = [1,2,3]
-let list2:Array<string> = ['s']
+let list: number[] = [1, 2, 3];
+let list2: Array<string> = ["s"];
 
-console.log(list,'list')
-console.log(list2,'list2')
+console.log(list, "list");
+console.log(list2, "list2");
+
+// 元组
+
+let x: [string, number];
+x = ["string", 123]; // ok
+// x = [123,'string'] // Error
+
+// 访问跨界元素 会使用联合类型替代
+x[3] = "hello";
+
+// x[2] = true // 布尔不是(string | number)
+console.log(x, "x");
+
+
+// 枚举
+
+enum Color {Red=1 , Green=2, Blue=5}
+
+let Colorq:string = Color[2]
+
+console.log(Colorq, 'colorQ')
+let colorB:number = Color.Blue
+console.log(colorB, 'colorB')
